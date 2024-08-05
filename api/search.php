03df48f9a081b285b92712ce257039aff72c2488
Miguel Marmolejo <95730640+MikeRDev-web@users.noreply.github.com>
2024-08-04 18:27:24 -0600

    Update search.php

--- api/search.php
+++ api/search.php
@@ -4,16 +4,15 @@ header('Access-Control-Allow-Origin: *'); // Permitir solicitudes desde cualquie
 header('Access-Control-Allow-Methods: GET'); // Métodos permitidos
 header('Access-Control-Allow-Headers: Content-Type'); // Encabezados permitidos
 
-require_once('./conection.php'); // Asegúrate de que este archivo configure la conexión PDO correctamente
+require_once('./conection.php'); 
 
 $searchTerm = filter_var($_GET['search'], FILTER_SANITIZE_SPECIAL_CHARS);
 
 try {
-    // Asegurarse de que la conexión PDO esté configurada en 'conection.php'
     $searchPartial = "%{$searchTerm}%";
     $consult = $conn->prepare('SELECT * FROM vehicles WHERE brand LIKE ? OR model LIKE ? OR year = ?');
     $consult->execute([$searchPartial, $searchPartial, $searchTerm]);
-    $resultSearch = $consult->get_result()->fetch_all(MYSQLI_ASSOC); // Usando PDO para obtener resultados como un array asociativo
+    $resultSearch = $consult->get_result()->fetch_all(MYSQLI_ASSOC);
 
     if ($resultSearch) {
         echo json_encode($resultSearch);
